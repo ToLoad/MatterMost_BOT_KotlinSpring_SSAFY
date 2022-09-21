9bcfffbca86bdd514c2d8cacf88c62e3e43fa5bd
Youngwon An <kublaihkan@gmail.com>
2022-09-21 09:21:31 +0900

    Add : First Init
    프로젝트 생성 및 gitignore 작성

---
+++ src/main/kotlin/com/ssafy/mmbot/MmbotApplication.kt
@@ -0,0 +1,11 @@
+package com.ssafy.mmbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MmbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/ssafy/mmbot/MmbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssafy.mmbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
